20cca160fc9e728d71356e17496a41f9a72c5ca5
squirrelandr <squirrelandr@gmail.com>
2015-06-21 19:00:17 -0400

    rprog-15 homework assignment #3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is to store a temporary inversed matrix in the environment
+## 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+## Not sure if these would work
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the value of the matrix
+  m <- null
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## get the value of the matrix
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+    data <- x$get()
+  m <- inverse(data, ...)
+  x$setinverse(m)
+  m
 }
