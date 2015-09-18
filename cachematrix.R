87258f80616bc2a344a4310e772d878085a2ab2a
Jian <squirrelandr@gmail.com>
2015-09-18 16:00:05 -0400

    Revert "rprog-15 homework assignment #3"
    This reverts commit 20cca160fc9e728d71356e17496a41f9a72c5ca5.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,15 @@
-## This function is to store a temporary inversed matrix in the environment
-## 
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function creates a special "matrix" object that can cache its inverse
-## Not sure if these would work
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## set the value of the matrix
-  m <- null
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  
-  ## get the value of the matrix
-  get <- function() x
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+
 }
 
 
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached matrix")
-    return(m)
-  }
-    data <- x$get()
-  m <- inverse(data, ...)
-  x$setinverse(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
